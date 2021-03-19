c2f01888b67e43e1df3435c7f428eb1da3374c0e
Josh Kraker <joshkrak@gmail.com>
2021-03-19 18:58:04 -0400

    Before switching to normal button

--- web/src/App.vue
+++ web/src/App.vue
@@ -20,7 +20,10 @@
                   >Docs</router-link>
                </li>
                <li class="nav-item">
-                  <button class="btn btn-link nav-link">Sign In</button>
+                  <button
+                     class="btn btn-link nav-link"
+                     @click="signin"
+                  >Sign In</button>
                </li>
             </ul>
 
@@ -32,25 +35,47 @@
          <router-view></router-view>
       </div>
 
-      <!-- <div
-         id="g_id_onload"
-         :data-client_id="clientId"
-         data-auto_select="true"
-         data-login_uri="https://005c84eba71548c5ac51ee853b95bf77.hook.events"
-      ></div> -->
-
    </div>
 </template>
 
 <script lang="ts">
    import { defineComponent } from 'vue';
-   import ButtonCopy from './components/global/ButtonCopy.vue';
+
+   declare var google: any;
 
    export default defineComponent({
-      components: { ButtonCopy },
       setup() {
-         const clientId = process.env['VUE_APP_GOOGLE_CLIENT_ID'];
-         return { clientId };
+
+         const signin = () => {
+
+            google.accounts.id.initialize({
+               client_id: process.env['VUE_APP_GOOGLE_CLIENT_ID'],
+               callback: (resp: any) => {
+                  console.log('google-login-response', resp);
+               }
+            });
+
+            google.accounts.id.prompt((notification: any) => {
+               console.log('google-notification', notification);
+               const moment: 'display' = notification.getMomentType();
+
+               if (moment === 'display') {
+                  const isDisplayed = notification.isDisplayed();
+                  if (!isDisplayed) {
+                     const reason = notification.getNotDisplayedReason();
+                     console.warn(`Failed to display signin - ${reason}`);
+                  }
+
+                  return;
+               }
+
+               console.warn(`Unknown moment - ${moment}`);
+
+            });
+
+         };
+
+         return { signin };
       },
    });
 </script>
